7045e1b5c14e709c93fcc4e1e1de1422589b3713
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-01-18 12:16:03 -0400

    feat: Executavel para linux

---
+++ linux/README.md
@@ -0,0 +1,6 @@
+# DB4K Linux
+
+O executável DB4K, desenvolvido para sistemas Linux e compatível com distribuições baseadas no Debian, destaca-se pela acessibilidade e facilidade de uso. Com uma abordagem amigável, o DB4K proporciona uma experiência intuitiva, tornando a aplicação acessível a usuários de diferentes níveis de habilidade no ambiente Linux.
+
+## Tutorial
+//video tutorial
---
+++ linux/src/exec/usr/local/bin/db4k.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "DEVELOPER BY DANIEL RODRIGUES"
+# CHECA SE O ARDUINO ESTÁ INSTALADO
+if ! command -v arduino &> /dev/null; then #CASO NÃO ESTEJA, INSTALA. 
+    echo "Instalando Arduino IDE"
+    wget https:downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz
+    rm -rf arduino-1.8.19-linux64.tar.xz
+    tar -xvf arduino-1.8.19-linux64.tar.xz
+    cd arduino-1.8.19
+
+fi
+
+#CHECA SE O PYTHON ESTA INSTALADO
+if ! command -v python2 &> /dev/null; then #CASO NÃO ESTEJA, ISTALA.
+    sudo apt-add-repository universe
+    sudo apt install python2-minimal
+
+fi
+
+#CHECA SE O REPO JÁ FOI CLONADO 
+if ! [ -e "./DB4K" ]; then #CASO NÃO TENHA SIDO, FAZ O GIT CLONE
+    git clone https://github.com/projetomymaker/DB4K
+
+fi
+
+# RODA O PROJETO
+if [ -e "./DB4K" ]; then
+    cd DB4K
+    sudo chmod 777 /dev/tty*
+    python2 start.py
+
+fi
+
